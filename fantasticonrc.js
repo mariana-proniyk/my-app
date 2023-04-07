5e4093a14dae59f16789af9a7864d131bf0557fd
Mariana Proniuk <meriproo@gmail.com>
2023-04-07 14:28:42 +0300

    feat: set up the development environment

---
+++ fantasticonrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	inputDir: "./assets/font-icons",
+	outputDir: "./public/fonts/icons",
+	assetTypes: ["scss", "ts"],
+	fontsUrl: "../../public/fonts/icons",
+	fontTypes: ["ttf", "woff", "woff2", "svg"],
+	pathOptions: {
+		scss: "./assets/scss/icons.scss",
+		ts: "./src/shared/types/icons.ts",
+	},
+};
