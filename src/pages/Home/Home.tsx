5067317db2872488acaa9633df5c65914c678673
Mariana Proniuk <meriproo@gmail.com>
2023-04-07 18:46:45 +0300

    feat: set up pages component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,9 +2,13 @@ import { Footer, Header } from "../../shared/components/grid";
 import {
 	AboutUsSection,
 	ClientSection,
+	ContactSection,
+	CounterSection,
 	HomeHeroSection,
+	ProjectSection,
 	WorkSection,
 } from "../../shared/components/sections";
+import { ArticlesSection } from "../../shared/components/sections/ArticlesSection/ArticlesSection";
 import { TestimonialSection } from "../../shared/components/sections/TestimonialSection/TestimonialSection";
 
 export const Home = () => {
@@ -16,6 +20,10 @@ export const Home = () => {
 			<AboutUsSection></AboutUsSection>
 			<TestimonialSection></TestimonialSection>
 			<ClientSection></ClientSection>
+			<ProjectSection></ProjectSection>
+			<CounterSection></CounterSection>
+			<ArticlesSection></ArticlesSection>
+			<ContactSection></ContactSection>
 			<Footer></Footer>
 		</>
 	);
