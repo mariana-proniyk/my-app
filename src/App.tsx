4d779000e3c99833e046f1e2ced1a10ae198d1db
Mariana Proniuk <meriproo@gmail.com>
2023-04-10 15:35:24 +0300

    feat: set up root file

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import "./App.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
+import { About } from "./pages/About/About";
+import { Services } from "./pages/Services/Services";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/services" element={<Services />} />
 			</Routes>
 		</BrowserRouter>
 	);
