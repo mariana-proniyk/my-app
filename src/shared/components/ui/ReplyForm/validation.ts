4c3965fa38da118390527a97a1e2dfbb70f088e5
Mariana Proniuk <meriproo@gmail.com>
2023-04-13 19:11:25 +0300

    feat: set up validation for form

--- src/shared/components/ui/ReplyForm/validation.ts
+++ src/shared/components/ui/ReplyForm/validation.ts
@@ -4,6 +4,6 @@ export const validationSchema = object({
 	email: string().email("*Email is required").required("*Email is not valid"),
 	name: string().min(3).max(10).required("*This field is required"),
 	message: string().min(15).required("*This field is required"),
-	number: string().min(10).required("*This field is required"),
+	tel: string().min(10).required("*This field is required"),
 	website: string().min(5).required("*This field is required"),
 });
